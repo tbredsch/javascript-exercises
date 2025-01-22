b11882c65ac71086a67d430382debc302fde4a55
tbredsch <tbredschneider@gmail.com>
2025-01-21 19:56:44 -0500

    Complete!!! I actually got it on my own without looking at the solution!

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(forString) {
+    let revString = "";
 
+    for (let i = -1; i >= -forString.length; i--) {
+        
+        revString += forString.at(i);
+    }
+return revString;
 };
 
 // Do not edit below this line
