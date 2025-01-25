17ee7daaed2ac4967f47c802c21787539692e6eb
tbredsch <tbredschneider@gmail.com>
2025-01-24 19:44:29 -0500

    Almost I'll have to come back to this but I passed the first two tests The next are more detailed.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function(int1, int2) {
+    let result = 0;
 
+    for (let i = int1; i <= int2; i++) {
+        result += i;
+    }
+
+    return result;
 };
 
 // Do not edit below this line
