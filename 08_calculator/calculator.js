a21f3a8fc464e044c5a00a514c6088a865b4ad67
tbredsch <tbredschneider@gmail.com>
2025-01-25 05:37:25 -0500

    Complete I was able to get most except for some of the fine details and the odd methods I had not seen before.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce(getSum, 0);
+
+  function getSum(total, current) {
+    return total + current;
+  }
 };
 
-const multiply = function() {
+const multiply = function(array) {
+  return array.reduce(getMult)
 
+  function getMult(total, current) {
+    return total * current
+  }
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return num1 ** num2
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let total = 1;
+
+  for(let i = n; i > 0; i--) {
+    total *= i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
